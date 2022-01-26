cfd88250216638edcacefb9eceeb97f07b926733
Mugerwa-Muhammad <mugerwa051@gmail.com>
2022-01-26 16:58:58 +0300

    teacher registration

---
+++ processes/connection.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "character_creation";
+
+$connection = mysqli_connect($server,$username,$password,$dbname);
+
+?>
---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>teacher register</title>
+</head>
+<body>
+    <h1>Teacher Register | Signup</h1>
+    <form action="register.php" method="post">
+        <input type="text" name="firstname" placeholder="Enter your firstname" required><br>
+        <input type="text" name="lastname" placeholder="Enter your lastname" required><br>
+        <input type="text" name="username" placeholder="Enter your username" required><br>
+        <input type="email" name="email" placeholder="Enter your email" required><br>
+        <input type="text" name="firstname" placeholder="Enter your password" required><br>
+        <input type="password" name="password" placeholder="Verify password" required><br>
+        <button name="signup_button" type="submit">Signup</button>
+    </form>
+    
+  
+</body>
+</html>

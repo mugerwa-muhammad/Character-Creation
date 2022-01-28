7b7399f2887ae2802862dfb6c1dca1976013300c
mugerwa-muhammad <98384492+mugerwa-muhammad@users.noreply.github.com>
2022-01-28 18:38:02 +0300

    index.php update

--- index.php
+++ index.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>teacher register</title>
+    <link rel="stylesheet" href="register.css">
 </head>
 <body>
-    <a href="addCharacterAssignment.php"> add </a>
+  <div class="container">
+    <h1>Teacher Register | Signup</h1><hr>
+    <div class="registration_form">
+      <form action="register.php" method="post">
+            <input type="text" name="firstname" placeholder="Enter your firstname" required="required"><br>
+            <input type="text" name="lastname" placeholder="Enter your lastname" required="required"><br>
+            <input type="text" name="username" placeholder="Enter your username" required><br>
+            <input type="email" name="email" placeholder="Enter your email" required="required"><br>
+            <input type="text" name="password1" placeholder="Enter your password" required="required"><br>
+            <input type="password" name="password2" placeholder="Verify password" required="required"><br>
+            <input type="submit" id="signup_button"name="signup_button" value="submit">
+       </form>
+    </div>
+  </div>
+    
+  
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+ include 'dbh.php';
+if(isset($_POST['signup_button'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+
+    
+    if($password1 != $password2){
+        header("Location:../index.php?signup=InvalidPassword!");
+    }else{
+
+        $sql = "INSERT INTO teachers(FirstName,LastName,UserName,email,passwrd) VALUES('$firstname','$lastname','$username','$email','$password2');";
+        mysqli_query($connection, $sql);
+        header("Location:./index.php?signup=success");
+    }
+
+}
+
+?>
+
